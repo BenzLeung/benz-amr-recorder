7cb3a27e8c6adc6bf5db3ceb3a2117969642c1bf
Benz <benz@youme.im>
2018-07-05 16:00:27 +0800

    pull request #3

--- BenzAMRRecorder.js
+++ BenzAMRRecorder.js
@@ -584,7 +584,7 @@ var curSourceNode = null;
 if (AudioContext) {
     ctx = new AudioContext();
 } else {
-    throw 'Web Audio API is Unsupported.';
+    console.error(new Error('Web Audio API is Unsupported.'));
 }
 /*
 const increaseSampleRate = function (samples, multiple) {
